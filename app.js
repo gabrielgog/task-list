4b0017505274d4d54e3fdd22b82617ba9ed51820
gabrielgog <gabrielgog96@gmail.com>
2020-11-15 01:17:58 +0100

    add js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-// Define UI Vars 
+// DEFINE UI VARS
 
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task');
-
 
 
 // Load all event listeners
 
-load
+function loadEventListeners(){
+
+    // Add task event
+
+    form.addEventListener('submit', addTask)
+};
+
+// Add Task
+
+function addTask(e){
+
+    if (taskInput.value === ''){
+
+        alert('Add a task')
+
+
+    }
+
+    e.preventDefault()
+
+}
